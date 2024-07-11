06ad990daad4c434a5f00c6a053c11bde250666b
박상훈 <152972679+sanghunii@users.noreply.github.com>
2024-07-11 22:18:40 +0900

    Update stack.c
    Fix segmentation fault when use DeleteNode()  function.
    Cause is findparent() inner support funtion
    
    
    When delete node that sandwiched between two nodes, occur segmentation fault.
    Cause is findparent() function.
    My previous findparent() funtion have 
                   parent = parent->prev; 
    this code. This code make to return the target node itself, not the parent node of the target node.

--- C/Stack_ADT/stack.c
+++ C/Stack_ADT/stack.c
@@ -1,3 +1,4 @@
+
 /* 인터페이스 구현 파일 */
 
 
@@ -10,7 +11,7 @@
 /* 내부 지원 함수 프로토타입 */
 static Node * searching(const Stack *, Item);    //Stack에서 원하는 정보를 가진 노드를 찾아서 그 노드의 주소를 리턴. (입력된 순서 역순으로 하나씩 뒤져봐야함.)
 static Node * makenode(Item item);          //새로운 노드를 만들어서 그 노드에 정보(item)을 저장하고 그 노드의 주소를 리턴.
-static Node * findparent(const Stack *, Item);    //그 노드를 가리키는 노드를 찾는다.
+static Node * findparent(const Stack *, const Item);    //그 노드를 가리키는 노드를 찾는다.
 static bool AddNode(Stack * pstack, Node * new_node);                       
 inline static void empty(void);              //버퍼를 비우는 인라인 함수.
 
@@ -71,6 +72,7 @@ bool Push(Stack * pstack, Item si)                                           //b
     return true;
 }
 
+
 void POP(Stack * pstack)
 {
     Node * temp;                        
@@ -96,44 +98,46 @@ void POP(Stack * pstack)
 }
 
 
+
 bool DeleteNode(Stack * pstack, Item si)
 {
     Node * t_node;                          //target node
-    Node * parent;                          //target node가 중간에 끼어있을때 사용.
-    int temp;
+    Node * parent;                          //target node가 중간에 껴있을 때 사용
+    //int temp;
     
-    if (!(t_node = searching(pstack, si)))
+    if (!(t_node = searching(pstack, si)))          //find target node using searching 
     {
         fprintf(stderr, "해당 정보를 저장하고 있는 노드가 존재하지 않습니다.\n");
         return false;
-    }
+    }                  
+
     /* 해당 정보를 저장하고 있는 노드가 존재함을 확인하면 */
     if (t_node == pstack->head)   //if target node is head node
     {
-        pstack->head = t_node->prev;                  
-        free(t_node);                                                
+        pstack->head = t_node->prev;                                                         
         pstack->size--;
     }
     else if (t_node->prev == NULL)              //if target node is tail node
     {
         parent = findparent(pstack, si);        //tail node의 부모노드를 찾는다.
-        free(t_node);                           //target node를 메모리에서 해제한다.
         parent->prev = NULL;                    //기존의 tail node의 부모노드를 tail node로 새로 지정.
         pstack->size--;
     }
     else   //target이 중간에 끼어 있으면
     {
-        parent = findparent(pstack, si);
-        parent->prev = t_node->prev;
-        free(t_node);
+        parent = findparent(pstack, si);        
+        parent->prev = t_node->prev;            
         pstack->size--;
     }
+
+    free(t_node);
 
     return true;
 }
 
 
-void DeleteAll(Stack * pstack)                  //use recursion
+
+void DeleteAll(Stack * pstack)
 {
     Node * pnode = pstack->head;                //Initializing pnode = Stack's head node              
     Node * tnode;                               //target node : 메모리에서 해제시킬 노드.
@@ -141,14 +145,17 @@ void DeleteAll(Stack * pstack)                  //use recursion
     while (pstack->size != 0)                 //pstack->size == 0이면 모든 노드들이 삭제되었다는 뜻이므로 해당 루프문의 조건문을 이렇게 짰다.
     {
         tnode = pnode;
-        pnode = pnode->prev;
+        pnode = pnode->prev; 
         free(tnode);
         pstack->size--;
     } 
+
+    Initialize(pstack);
 }       
 /**** recursion을 이용한 DeleteAll()구현에서는 전달인자를 Stack * 으로 다른 함수 인터페이스들과 일관성을 맞추기 힘들다. 
- *      따라서 다른 함수 인터페이스들과의 일관ㅌ을 위해서 recursion이 아닌 다른 방법으로 DeleteAll()을 구현해야한다.
+ *      따라서 다른 함수 인터페이스들과의 일관성을 위해서 recursion이 아닌 다른 방법으로 DeleteAll()을 구현해야한다.
 */
+
 
 void InStack(Stack * pstack, Item si)
 {
@@ -201,20 +208,25 @@ bool GetItem(Item * si)
 
 
 /* ***** 지원 함수 ***** */
-static Node * findparent(const Stack * pstack, Item si)
+static Node * findparent(const Stack * pstack, const Item si)
 {
     Node * parent;
     
     parent = pstack->head;
     
-    while (parent->prev)     //parent가 마지막 노드가 아니면 계속 루프 진행.
-        if (parent->prev->item == si)
+    while (parent->prev) {
+        if (parent->prev->item == si) {
+            //parent = parent->prev;
             break;
-        else
-            continue;           //if-else문은 하나의 문장으로 C가 받아들이기 때문에 중괄호 필요 없다 하지만 if문이 while()루프의 몸체라는 사실을 명확히 하기 위해 중괄홀르 씌워주자.         
+        }
+        else {
+            parent = parent->prev;
+            continue;
+        }       
+    }
     
 
-    return parent;              //리턴값이 NULL이면 si를 가리키는 노드를 가리키는 노드가 없음.
+    return parent;           
 }
 
 static Node * searching(const Stack * pstack, Item si)
@@ -222,14 +234,15 @@ static Node * searching(const Stack * pstack, Item si)
     Node * temp;
      
     temp = pstack->head;
-    while (temp != NULL)
+    while (temp != NULL) {
         if (temp->item == si)
             break;
         else
-            temp = temp->prev;          //if - else문은 전체가 하나의 문장으로 카운트 된다.
+            temp = temp->prev;
+    }
 
     return temp;                    
-}   //원하는 item이 들어있는 노드를 찾으면 그 노드를 리턴. Item을 못찾았으면 temp의 값은 NULL
+}
 
 static Node * makenode(Item si)             //si : stack item
 {
